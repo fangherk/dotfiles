874c5c22bf76318f4cfde30864e1118dfc50f469
Herrick Fang <herrickfang@gmail.com>
2018-02-27 08:08:58 +0100

    switch from soft links to hard links

--- emacs/init.el
+++ emacs/init.el
@@ -1,1 +1,269 @@
-/home/herk/.emacs.d/init.el
+;; Herrick's Emacs Config 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Package defaults 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(require 'package)
+(add-to-list 'package-archives
+       '("melpa" . "http://melpa.org/packages/") t)
+
+(package-initialize) ;; initialize installed packages linked with require
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Important Packages
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defvar myPackages
+  '(helm
+    helm-swoop
+    evil
+    semantic
+    smart-mode-line
+    projectile
+    recentf
+    better-defaults
+    zenburn-theme))
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Basic Customization
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq inhibit-startup-message t) ;; Remove the original startup message
+(load-theme 'zenburn t) ;; Load Zenburn Theme
+(global-linum-mode t) ;; Add line numbers 
+(global-visual-line-mode t) ;; Make words wrap
+(menu-bar-mode -1) ;; Remove menu bar
+(toggle-scroll-bar -1) ;; Remove toggle scroll bar
+(tool-bar-mode -1) ;; Remove toll bar
+(setq column-number-mode t) ;; Add column number
+
+;; Smart Status bar
+(setq sml/theme 'dark)
+(setq sml/no-confirm-load-theme t)
+(sml/setup)
+
+;; Improve window switching
+(defun frame-bck()
+  (interactive)
+  (other-window -1)
+)
+(define-key (current-global-map) (kbd "M-o") 'other-window)
+(define-key (current-global-map) (kbd "M-O") 'frame-bck)
+
+;; Map switching windows with shift + arrows
+(windmove-default-keybindings)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Helm Mode settings 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'helm)
+(require 'helm-config)
+(require 'helm-eshell)
+
+;; add eshell
+(add-hook 'eshell-mode-hook
+          #'(lambda ()
+              (define-key eshell-mode-map (kbd "C-c C-l")  'helm-eshell-history)))
+
+;; Key Bindings
+(global-set-key (kbd "M-x") 'helm-M-x) ;; add global helm keybinding
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c")) ;; remove this key because it might close Emacs prematurely
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks) ;; not useful?
+(global-set-key (kbd "C-x C-f") #'helm-find-files) ;; find files -- master command
+(global-set-key (kbd "M-y") 'helm-show-kill-ring) ;; Very helpful last cuts
+(global-set-key (kbd "C-x C-r") 'helm-recentf) ;; Get the recent files
+(global-set-key (kbd "C-c h o") 'helm-occur) ;; Check occurences of words in file
+(global-set-key (kbd "C-h SPC") 'helm-all-mark-rings) ;; Possibly useful binding?
+(global-set-key (kbd "C-c h x") 'helm-register) ;; view registers
+;; rebind keys
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+;; helm-man-woman
+(add-to-list 'helm-sources-using-default-as-input 'helm-source-man-pages)
+
+
+(when (executable-find "ack-grep")
+  (setq helm-grep-default-command "ack-grep -Hn --no-group --no-color %e %p %f"
+        helm-grep-default-recurse-command "ack-grep -H --no-group --no-color %e %p %f"))
+
+;; Enable fuzziness.
+(setq helm-buffers-fuzzy-matching t
+      helm-locate-fuzzy-match t
+      helm-lisp-fuzzy-completion t
+      helm-apropos-fuzzy-match t
+      helm-recentf-fuzzy-match    t)
+
+;; Something with curl?
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+;; Add some better windowing
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t
+      helm-echo-input-in-header-line t)
+
+;; helm-swoop defaults
+(require 'helm-swoop)
+
+;; Change keybinds to whatever you like :)
+(global-set-key (kbd "M-i") 'helm-swoop)
+(global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
+(global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
+(global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
+
+;;When doing isearch, hand the word over to helm-swoop
+(define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
+(define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
+
+;; When doing evil-search, hand the word over to helm-swoop
+;;(define-key evil-motion-state-map (kbd "M-i") 'helm-swoop-from-evil-search)
+
+;; Instead of helm-multi-swoop-all, you can also use helm-multi-swoop-current-mode
+;;(global-set-key helm-swoop-map (kbd "M-m") 'helm-multi-swoop-current-mode-from-helm-swoop)
+
+;; Move up and down like isearch
+(define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
+(define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
+(define-key helm-multi-swoop-map (kbd "C-r") 'helm-previous-line)
+(define-key helm-multi-swoop-map (kbd "C-s") 'helm-next-line)
+
+;; Save buffer when helm-multi-swoop-edit complete
+(setq helm-multi-swoop-edit-save t)
+
+;; If this value is t, split window inside the current window
+(setq helm-swoop-split-with-multiple-windows t)
+
+;; Split direcion. 'split-window-vertically or 'split-window-horizontally
+(setq helm-swoop-split-direction 'split-window-vertically)
+
+;; If nil, you can slightly boost invoke speed in exchange for text color
+(setq helm-swoop-speed-or-color nil)
+
+;; ;; Go to the opposite side of line from the end or beginning of line
+(setq helm-swoop-move-to-line-cycle t)
+
+;; Optional face for line numbers
+;; Face name is `helm-swoop-line-number-face`
+(setq helm-swoop-use-line-number-face t)
+
+;; If you prefer fuzzy matching
+(setq helm-swoop-use-fuzzy-match t)
+
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 30)
+(helm-autoresize-mode 1)
+
+
+(helm-mode 1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Save recent files
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 30)
+(run-at-time nil (* 5 30) 'recentf-save-list)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Evil Mode for vim layer at startup
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(evil-mode 1)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Semantic Mode 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(semantic-mode 1)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Add Org-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Disable the splash screen (to enable it agin, replace the t with 0)
+(setq inhibit-splash-screen t)
+
+;; Enable transient mark mode
+(transient-mark-mode 1)
+
+;;;;Org mode configuration
+;; Enable Org mode
+;; Addd org-mode bindings
+(require 'org)
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Projectile-Mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(projectile-global-mode)
+(add-hook 'ruby-mode-hook 'projectile-mode)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Add Prolog directory
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-to-list 'load-path "~/.emacs.d/prolog/")
+(autoload 'run-prolog "prolog" "Start a Prolog sub-process." t)
+(autoload 'prolog-mode "prolog" "Major mode for editing Prolog programs." t)
+(setq prolog-system 'swi) ; prolog-system below for possible values
+(add-to-list 'auto-mode-alist '("\\.pl\\'" . prolog-mode))
+
+(require 'ediprolog)
+(global-set-key [f10] 'ediprolog-dwim)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; store all backup and autosave files in the tmp dir
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" , temporary-file-directory t)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; End basic init
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(org-agenda-files (quote ("~/Dropbox/todo/today.org")))
+ '(package-selected-packages
+   (quote
+    (smart-mode-line helm-swoop helm-projectile helm ediprolog projectile zenburn-theme org material-theme better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- vim/.vimrc
+++ vim/.vimrc
@@ -1,1 +1,96 @@
-/home/herk/.vimrc
+"" Herrick's Custom Vimrc file
+execute pathogen#infect()
+set t_Co=256
+
+
+""" C config security 
+set secure              " restrict usage of certain commands
+
+""" set UTF-8 encoding
+set enc=utf-8
+set fenc=utf-8
+set termencoding=utf-8
+""" Start with the color scheme
+""" colorscheme molokai	    " Basic Color Scheme, ~/.vim/colors/xx.vim
+
+""" Basic Processing
+syntax enable   	    " enable syntax processing
+
+""" Indent
+set autoindent          " set some automatic indenting
+set cindent             " set c indenting
+
+""" Spaces & Tabs
+set tabstop=4		    " number of visual spaces per TAB
+set softtabstop=4	    " number of spaces in tab when editing
+set shiftwidth=4        " useful for shiftwidths
+set expandtab		    " tabs are spaces
+
+" white spaces deleted on save
+""" autocmd BufWritePre * :%s/\s\+$//e
+
+""" UI Config
+""" set colorcolumn=110     " create a diff color after 110 characters
+highlight ColorColumn ctermbg=darkgray
+set number		        " show line numbers
+set showcmd 		    " show command in bottom bar
+set cursorline		    " highlight current line
+filetype plugin on      " add vim built-in plugins
+""" filetype indent on 	    " load filetype-specific indent files, ~/.vim/indent/xx.vim
+set wildmenu		    " visual autocomplete for command menu
+set lazyredraw		    " redraw only when we need to
+
+""" Searching
+set incsearch		    " search as characters are entered
+set hlsearch		    " highlight matches
+
+" turn off search highlight
+nnoremap <ESC><ESC> :nohlsearch<CR>
+
+""" Folding
+set foldenable          " enable folding
+set foldlevelstart=10   " open most folds by default
+set foldnestmax=10      " 10 nested fold max
+set foldmethod=indent   " fold based on marker level
+
+" space open/closes folds
+nnoremap <space> za
+
+""" Movement
+
+" move vertically by visual line
+" nnoremap j gj
+" nnoremap k gk
+
+
+""" TMUX Stuff
+" allows cursor change in tmux mode
+if exists('$TMUX')
+    let &t_SI = "\<Esc>Ptmux;\<Esc>\<Esc>]50;CursorShape=1\x7\<Esc>\\"
+    let &t_EI = "\<Esc>Ptmux;\<Esc>\<Esc>]50;CursorShape=0\x7\<Esc>\\"
+else
+    let &t_SI = "\<Esc>]50;CursorShape=1\x7"
+    let &t_EI = "\<Esc>]50;CursorShape=0\x7"
+endif
+
+"""Remap list buffers to F5
+:nnoremap <F5> :buffers<CR>:buffer<Space>
+
+""" Map leader key 
+:let mapleader = ","
+
+""" Shortcut to reload vimrc
+map <leader>s :source ~/.vimrc<CR>
+
+""" Re-open previously opened file
+nnoremap <Leader><Leader> :e#<CR>
+
+""" Add some useful commands
+map <C-n> :NERDTreeToggle<CR>
+
+""" add to system clipboard
+set clipboard=unnamedplus
+
+""" add support
+autocmd FileType apache setlocal commentstring=#\ %s
+
